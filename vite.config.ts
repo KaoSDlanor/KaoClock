4607b1092f690f411e0714dde5ed00c56a326e12
kaosdlanor <kaosdlanor@gmail.com>
2021-11-20 21:00:39 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import Path             from 'path';
+import { defineConfig } from 'vite';
+import SolidPlugin      from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [SolidPlugin()],
+  resolve:{
+    alias:{
+      '@' : Path.resolve(__dirname, './src')
+    },
+  },
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+  },
+});
